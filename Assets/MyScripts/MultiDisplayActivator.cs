013a70e81bea3d4d66bf8dc3d2ce7d280e4de3fd
Tommy Gil <tommygilsanagustin@gmail.com>
2025-12-02 12:22:34 +0100

    Codigos Myscripts final

--- Assets/MyScripts/MultiDisplayActivator.cs
+++ Assets/MyScripts/MultiDisplayActivator.cs
@@ -1,12 +1,12 @@
-using UnityEngine;
+// using UnityEngine;
 
-public class MultiDisplayActivator : MonoBehaviour
-{
-    void Start()
-    {
-        for (int i = 1; i < Display.displays.Length; i++)
-        {
-            Display.displays[i].Activate();
-        }
-    }
-}
+// public class MultiDisplayActivator : MonoBehaviour
+// {
+//     void Start()
+//     {
+//         for (int i = 1; i < Display.displays.Length; i++)
+//         {
+//             Display.displays[i].Activate();
+//         }
+//     }
+// }
